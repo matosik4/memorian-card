0607e22ea3cf9782e82c0d2c172e8b04a7c198b2
matosik <120247151+matosik4@users.noreply.github.com>
2024-09-14 11:34:49 +0300

    Add files via upload

---
+++ memo_main.py
@@ -0,0 +1,11 @@
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QWidget
+
+from memo_app import app
+from memo_data import *
+from memo_main_layout import *
+from memo_card_layout import *
+from memo_edit_layout import txt_Question, txt_Answer, txt_Wrong1, txt_Wrong2, txt_Wrong3
+
+
+app.exec_()
